7b5ad967a4e9f4af8d605fae8ac6acf8bf478c11
ArthurVinicius <164905139+Arthurvnss@users.noreply.github.com>
2026-04-15 08:05:26 -0300

    Create att6.js

---
+++ att6.js
@@ -0,0 +1,7 @@
+var missao1 = prompt("Digite uma missão")
+var missao2 = prompt("Digite outra missão")
+
+document.write("Missões:<br>")
+document.write("- " + missao1 + "<br>")
+document.write("- " + missao2 + "<br>")
+
