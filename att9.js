32ff6b75336db4b4e56c0f0c818d1b0e2de290be
ArthurVinicius <164905139+Arthurvnss@users.noreply.github.com>
2026-04-15 08:07:40 -0300

    Create att9.js

---
+++ att9.js
@@ -0,0 +1,16 @@
+// DESAFIO 9
+var ana = 0
+var joao = 0
+var carlos = 0
+
+var voto = prompt("Vote: ana, joao ou carlos")
+
+if(voto == "ana"){ ana++ }
+if(voto == "joao"){ joao++ }
+if(voto == "carlos"){ carlos++ }
+
+document.write("Ana: " + ana + "<br>")
+document.write("João: " + joao + "<br>")
+document.write("Carlos: " + carlos + "<br>")
+
+
